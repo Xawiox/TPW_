08a96cb6e0847758739561d3bfd46188ca9ac4c7
DamianBednarczyk251483 <251483@edu.p.lodz.pl>
2025-04-12 19:51:04 +0200

    Etap 1 - poprawianie błedu z move

--- Data/DataImplementation.cs
+++ Data/DataImplementation.cs
@@ -13,79 +13,80 @@ using System.Diagnostics;
 
 namespace TP.ConcurrentProgramming.Data
 {
-  internal class DataImplementation : DataAbstractAPI
-  {
-    #region ctor
-
-    public DataImplementation()
+    internal class DataImplementation : DataAbstractAPI
     {
+        #region ctor
+
+        public DataImplementation()
+        {
             //MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(10));
+
+        }
+
+        #endregion ctor
+
+        #region DataAbstractAPI
+
+        public override void Start(int numberOfBalls, Action<IVector, IBall> upperLayerHandler)
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(DataImplementation));
+            if (upperLayerHandler == null)
+                throw new ArgumentNullException(nameof(upperLayerHandler));
+            Random random = new Random();
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                Vector startingPosition = new(random.Next(100, 400 - 100), random.Next(100, 400 - 100));
+                double xVelocity = random.Next(-80, 80);
+                double yVelocity = Math.Sqrt(6400 - xVelocity * xVelocity);
+                double initialDiameter = random.Next(10, 40);
+                Vector startingVelocity = new(xVelocity, yVelocity);
+                Ball newBall = new(startingPosition, startingVelocity, initialDiameter);
+                upperLayerHandler(startingPosition, newBall);
+                BallsList.Add(newBall);
+            }
             StartGameLoop();
         }
 
-    #endregion ctor
+        #endregion DataAbstractAPI
 
-    #region DataAbstractAPI
+        #region IDisposable
 
-    public override void Start(int numberOfBalls, Action<IVector, IBall> upperLayerHandler)
-    {
-      if (Disposed)
-        throw new ObjectDisposedException(nameof(DataImplementation));
-      if (upperLayerHandler == null)
-        throw new ArgumentNullException(nameof(upperLayerHandler));
-      Random random = new Random();
-      for (int i = 0; i < numberOfBalls; i++)
-      {
-            Vector startingPosition = new(random.Next(100, 400 - 100), random.Next(100, 400 - 100));
-            double xVelocity = random.Next(-80, 80);
-            double yVelocity = Math.Sqrt(6400 - xVelocity * xVelocity);
-            double initialDiameter = random.Next(10, 40);
-            Vector startingVelocity = new(xVelocity, yVelocity);
-            Ball newBall = new(startingPosition, startingVelocity, initialDiameter);
-            upperLayerHandler(startingPosition, newBall);
-            BallsList.Add(newBall);
-      }
-    }
-
-    #endregion DataAbstractAPI
-
-    #region IDisposable
-
-    protected virtual void Dispose(bool disposing)
-    {
-      if (!Disposed)
-      {
-        if (disposing)
+        protected virtual void Dispose(bool disposing)
         {
-          BallsList.Clear();
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    BallsList.Clear();
+                }
+                Disposed = true;
+            }
+            else
+                throw new ObjectDisposedException(nameof(DataImplementation));
         }
-        Disposed = true;
-      }
-      else
-        throw new ObjectDisposedException(nameof(DataImplementation));
-    }
 
-    public override void Dispose()
-    {
-      // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-      Dispose(disposing: true);
-      GC.SuppressFinalize(this);
-    }
+        public override void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
 
-    #endregion IDisposable
+        #endregion IDisposable
 
-    #region private
+        #region private
 
-    //private bool disposedValue;
-    private bool Disposed = false;
+        //private bool disposedValue;
+        private bool Disposed = false;
 
-    private List<Ball> BallsList = [];
+        private List<Ball> BallsList = [];
 
-    private void Move(double deltaTime)
-    {
-      foreach (Ball item in BallsList)
-        item.Move(new Vector(item.Velocity.x * deltaTime, item.Velocity.y * deltaTime));
-    }
+        private void Move(double deltaTime)
+        {
+            foreach (Ball item in BallsList)
+                item.Move(new Vector(item.Velocity.x * deltaTime, item.Velocity.y * deltaTime));
+        }
 
         private void StartGameLoop()
         {
@@ -113,23 +114,23 @@ namespace TP.ConcurrentProgramming.Data
         #region TestingInfrastructure
 
         [Conditional("DEBUG")]
-    internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
-    {
-      returnBallsList(BallsList);
-    }
+        internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
+        {
+            returnBallsList(BallsList);
+        }
 
-    [Conditional("DEBUG")]
-    internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
-    {
-      returnNumberOfBalls(BallsList.Count);
-    }
+        [Conditional("DEBUG")]
+        internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
+        {
+            returnNumberOfBalls(BallsList.Count);
+        }
 
-    [Conditional("DEBUG")]
-    internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
-    {
-      returnInstanceDisposed(Disposed);
-    }
+        [Conditional("DEBUG")]
+        internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
+        {
+            returnInstanceDisposed(Disposed);
+        }
 
-    #endregion TestingInfrastructure
-  }
+        #endregion TestingInfrastructure
+    }
 }
